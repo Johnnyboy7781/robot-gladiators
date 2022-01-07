1f716deae1ddce5b3adae3f1df34802dbb6ddd12
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2022-01-06 19:46:14 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,5 @@
 var playerName = window.prompt("What is your robot's name?");
-var playerHealth = 10;
+var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
@@ -63,7 +63,12 @@ var fight = function(enemyName) {
     }
 };
 
-for (var i = 0; i < enemyNames.length; i++) {
+var startGame = () => {
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
+  for (var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
         window.alert(`Welcome to Robot Gladiators! Round ${i + 1}`);
         var pickedEnemyName = enemyNames[i];
@@ -73,4 +78,25 @@ for (var i = 0; i < enemyNames.length; i++) {
         window.alert("You have lost your robot in battle! Game Over!");
         break;
     }
+  }
+
+  endGame();
 }
+
+var endGame = () => {
+  if (playerHealth > 0) {
+    window.alert(`Great job, you've survived the game! You now have a score of ${playerMoney}.` );
+  } else {
+    window.alert(`You've lost your robot in battle.`);
+  }
+
+  var playAgainConfirm = window.confirm(`Would you like to play again?`);
+
+  if (playAgainConfirm) {
+    startGame();
+  } else {
+    window.alert(`Thank you for playing Robot Gladiators! Come back soon!`)
+  }
+}
+
+startGame();
